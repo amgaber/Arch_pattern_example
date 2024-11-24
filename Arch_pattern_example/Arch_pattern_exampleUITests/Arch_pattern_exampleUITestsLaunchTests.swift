9d0b7e435275b088be57d3210ee35a2db6617018
Alaa <alaamohammedgaber@gmail.com>
2024-11-24 13:22:32 +0200

    first commit
    just create a project

---
+++ Arch_pattern_example/Arch_pattern_exampleUITests/Arch_pattern_exampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Arch_pattern_exampleUITestsLaunchTests.swift
+//  Arch_pattern_exampleUITests
+//
+//  Created by Alaa Gaber on 24/11/2024.
+//
+
+import XCTest
+
+final class Arch_pattern_exampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
